275037ec98f9d4687ad45ba94bac681f16a09e05
semaali159 <semaali1599@gmail.com>
2025-04-13 07:15:58 -0700

    v10

--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,15 @@
-//development
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-  }
-);
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log(
-      "Connection to the database has been established successfully."
-    );
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-})();
-
-module.exports = sequelize;
-
-//production
+// //development
 // const { Sequelize } = require("sequelize");
 // require("dotenv").config();
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true, // يفرض استخدام SSL
-//       rejectUnauthorized: false, // يسمح بالاتصال بشهادات SSL غير موثوقة (تُستخدم عادةً مع Render)
-//     },
-//   },
-// });
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_NAME,
+//   process.env.DATABASE_USERNAME,
+//   process.env.DATABASE_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "postgres",
+//   }
+// );
 
 // (async () => {
 //   try {
@@ -50,3 +23,30 @@ module.exports = sequelize;
 // })();
 
 // module.exports = sequelize;
+
+production;
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // يفرض استخدام SSL
+      rejectUnauthorized: false, // يسمح بالاتصال بشهادات SSL غير موثوقة (تُستخدم عادةً مع Render)
+    },
+  },
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = sequelize;
