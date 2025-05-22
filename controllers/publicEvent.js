b7eee5c55fcd59c84c25ef1e3d8a2afa226ad1af
semaali159 <semaali1599@gmail.com>
2025-05-22 14:45:23 -0700

    v14

--- controllers/publicEvent.js
+++ controllers/publicEvent.js
@@ -14,6 +14,32 @@ const createEvent = asyncHandler(async (req, res) => {
     longitude,
     price,
     tickets,
+    userId: req.user.id,
   });
+  //send notificaton for all followers
   return res.status(201).json({ message: "event created successfuly" });
+});
+const getAllEvents = asyncHandler(async (req, res) => {
+  const events = db.publicEvent.findAll({
+    attributes: ["id", "name", "image"],
+  });
+  if (!events) {
+    return res.status(404).json({ message: "events not found" });
+  }
+  return res.status(404).json({ message: "events: ", events });
+});
+
+const getEventById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const event = db.publicEvent.findByPk(id, {
+    include: [{ model: db.user, attributes: ["id", "name", "image"] }],
+  });
+  if (!event) {
+    return res.status(404).json({ message: "event not found" });
+  }
+  return res.status(404).json({ message: "events: ", event });
+});
+const getUpComingEvent = asyncHandler(async (req, res) => {
+  const currentTime = Date.now();
+  const date = new Date(currentTime);
 });
