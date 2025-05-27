e52fac2b325a0fbdc9b22c59d8069462aa6ac3f1
semaali159 <semaali1599@gmail.com>
2025-05-26 21:34:44 -0700

    v15

--- config/connection.js
+++ config/connection.js
@@ -1,42 +1,15 @@
 // //development
-const { Sequelize } = require("sequelize");
-require("dotenv").config();
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USERNAME,
-  process.env.DATABASE_PASSWORD,
-  {
-    host: "localhost",
-    dialect: "postgres",
-  }
-);
-
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log(
-      "Connection to the database has been established successfully."
-    );
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-})();
-
-module.exports = sequelize;
-
-// // production;
 // const { Sequelize } = require("sequelize");
 // require("dotenv").config();
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   protocol: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_NAME,
+//   process.env.DATABASE_USERNAME,
+//   process.env.DATABASE_PASSWORD,
+//   {
+//     host: "localhost",
+//     dialect: "postgres",
+//   }
+// );
 
 // (async () => {
 //   try {
@@ -50,3 +23,31 @@ module.exports = sequelize;
 // })();
 
 // module.exports = sequelize;
+
+// production;
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+const sequelize = new Sequelize(process.env.NEON, {
+  // const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+})();
+
+module.exports = sequelize;
